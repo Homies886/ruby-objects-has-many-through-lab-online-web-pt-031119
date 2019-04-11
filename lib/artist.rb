c02380546ea58c09b320b36e595fce320ae2c637
Nicholas Rodriguez <niko876@hotmail.com>
2019-04-11 01:36:23 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,7 +14,8 @@ class Artist
   end  
   
   def new_song(name, genre)
-    song.new(name, self, genre)
+    song = song.new(name, self, genre)
+    song
   end
   
   def songs
@@ -24,7 +25,8 @@ class Artist
   end
   
   def genres
-    
+    songs.map do |genre|
+      
   end
   
 end  
