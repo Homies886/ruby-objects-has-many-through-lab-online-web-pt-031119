276e11410edcc770eb529f1226c5ae1360a353f6
Nicholas Rodriguez <niko876@hotmail.com>
2019-04-11 02:06:21 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -14,11 +14,14 @@ class Patient
   end
   
   def new_appointment(doctor, date)
-    
+    appointment = Appointment.new(date, self, doctor)
+    appointment
   end
   
   def appointments
-    
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end  
   end
   
   def doctors
